ff637be01357b22cc3e9823304b87b355a92f7f6
Kacper Siwoń <152258422+Kax3per@users.noreply.github.com>
2023-12-06 09:51:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -258,5 +258,92 @@ async function searchImages() {
 
 
 //WEATHER APP
+const wrapper = document.querySelector(".wrapper"),
+inputPart = document.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = wrapper.querySelector(".weather-part"),
+arrowBack = wrapper.querySelector("header i")
 
+apiKey = "02b45911ed33529e70c9a0baffd21d0e"; // Replace with your actual API key
+let api;
+
+inputField.addEventListener("keyup", (e) => {
+if (e.key == "Enter" && inputField.value != "") {
+  requestApi(inputField.value);
+}
+});
+
+locationBtn.addEventListener("click", () => {
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+} else {
+  alert("Your browser does not support geolocation api");
+}
+});
+
+function requestApi(city) {
+api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+fetchData();
+}
+
+function onSuccess(position) {
+const { latitude, longitude } = position.coords;
+api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+fetchData();
+}
+
+function onError(error) {
+infoTxt.innerText = error.message;
+infoTxt.classList.add("error");
+}
+
+function fetchData() {
+infoTxt.innerText = "Getting weather details...";
+infoTxt.classList.add("pending");
+fetch(api)
+  .then((res) => res.json())
+  .then((result) => weatherDetails(result))
+  .catch(() => {
+    infoTxt.innerText = "Something went wrong, API Error";
+    infoTxt.classList.replace("pending", "error");
+  });
+}
+
+function weatherDetails(info) {
+if (info.cod == "404") {
+  infoTxt.classList.replace("pending", "error");
+  infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+} else {
+  const city = info.name;
+  const country = info.sys.country;
+  const { description, id } = info.weather[0];
+  const { temp } = info.main;
+
+  // Kody do wyboru ikony zostały zachowane dla Twojej referencji
+
+  weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+  weatherPart.querySelector(".weather").innerText = description;
+  weatherPart.querySelector(
+    ".location span"
+  ).innerText = `${city}, ${country}`;
+
+  infoTxt.classList.remove("pending", "error");
+  infoTxt.innerText = "";
+  inputField.value = "";
+  wrapper.classList.add("active");
+
+  arrowBack.addEventListener("click", () => {
+wrapper.classList.remove("active");
+});
+
+}
+}   
  
+
+
+
+// BOOK
+
+
